1d7800766597471eafd0b84d3c812e434cfeaef7
Yamila Lerman <yamilerman@gmail.com>
2024-04-30 12:01:54 -0300

    juego v1

---
+++ script.js
@@ -0,0 +1,31 @@
+//El juego selecciona un número al azar para que adivines
+let numeroAzar = Math.floor(Math.random() * 100) + 1;
+
+let numeroEntrada = document.getElementById("numeroEntrada");
+let mensaje = document.getElementById("mensaje");
+let intento = document.getElementById("intento");
+let intentos = 0;
+
+//Esta acción se va a ejecutar cuando se toque el botón chequear
+function chequearResultado() {
+  intentos++;
+  intento.textContent = intentos;
+  let numeroIngresado = parseInt(numeroEntrada.value);
+  if (numeroIngresado < 1 || numeroIngresado > 100 || isNaN(numeroIngresado)) {
+    mensaje.textContent = "Por favor introduce un número válido entre 1 y 100";
+    mensaje.style.color = "red";
+    return;
+  }
+
+  if (numeroIngresado === numeroAzar) {
+    mensaje.textContent = "¡Felicitaciones! ¡Has adivinado el número!";
+    mensaje.style.color = "green";
+    numeroEntrada.disabled = true;
+  } else if (numeroIngresado < numeroAzar) {
+    mensaje.textContent = "¡Más alto! El número es mayor al que dijiste";
+    mensaje.style.color = "red";
+  } else {
+    mensaje.textContent = "¡Más bajo! El número es menor al que dijiste";
+    mensaje.style.color = "red";
+  }
+}
